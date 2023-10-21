8cc1a09b3dd5adfd3cad50f606d98e594fdb3ce2
Mohammed Alazaar <mhmdalazr@icloud.com>
2023-10-21 14:28:03 +0300

    a lot

--- JS/change-password.js
+++ JS/change-password.js
@@ -1,15 +1,21 @@
 $(document).ready(function () {
-    $(".edit-button2").click(function () {
-        const password = $(".Password");
-        password.replaceWith('<input type="password" class="Password" value="' + password.text() + '">');
-        $(".edit-button2").css('display', 'none');
-        $("#saveButton2").css('display', 'block');
+    $(".edit-button").click(function () {
+        const email = $(".Password");
+        const phone = $(".confraim-Password-textarea");
+        email.replaceWith('<input type="text" class="Password" value="' + email.text() + '">');
+        phone.replaceWith('<input type="text" class="confraim-Password-textarea" value="' + phone.text() + '">');
+        $(".edit-button").css('display', 'none');
+        $(".sendButton").css('display', 'block');
     });
 
-    $("#saveButton2").click(function () {
-        const passwordInput = $(".Password");
-        passwordInput.replaceWith('<p class="Password">' + passwordInput.val() + '</p>');
-        $(".edit-button2").css('display', 'block');
-        $("#saveButton2").css('display', 'none');
+    $("#saveButton").click(function () {
+        const emailInput = $(".Password");
+        const phoneInput = $(".confraim-Password-textarea");
+        const newEmail = emailInput.val();
+        const newPhone = phoneInput.val();
+        emailInput.replaceWith('<p class="Password">' + newEmail + '</p>');
+        phoneInput.replaceWith('<p class="confraim-Password-textarea">' + newPhone + '</p>');
+        $(".edit-button").css('display', 'block');
+        $(".sendButton").css('display', 'none');
     });
 });
